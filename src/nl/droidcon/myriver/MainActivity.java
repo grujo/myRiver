46da30d15be460fb37746acbd0672e6120cd5b57
Maxim Tkachenko <maxim@2dehands.be>
2013-11-24 12:06:20 +0100

    Simple gedture detection code.

--- src/nl/droidcon/myriver/MainActivity.java
+++ src/nl/droidcon/myriver/MainActivity.java
@@ -2,8 +2,13 @@ package nl.droidcon.myriver;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends Activity {
+import it.imwatch.SimpleShakeDetector;
+
+public class MainActivity extends Activity implements SimpleShakeDetector.OnShakeListener {
+    private SimpleShakeDetector simpleShakeDetector;
+
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +16,31 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        simpleShakeDetector = new SimpleShakeDetector(this, this, 100);
+        simpleShakeDetector.setMinGestureSize(1);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        simpleShakeDetector.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        simpleShakeDetector.onPause();
+        super.onPause();
+    }
+
+    private int shakedTimes = 0;
+
+    @Override
+    public void onShakeDetected(int i) {
+        if (shakedTimes++ >= 3) {
+            Log.i("MR", "Rock!");
+            shakedTimes = 0;
+        }
     }
 }
