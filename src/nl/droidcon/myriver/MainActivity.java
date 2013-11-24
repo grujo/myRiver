e0a928f7ba1508c26a159f2bf9570b4794f0c7d8
lukasz <lukasz.izmajlowicz@mydriver.com>
2013-11-24 12:20:01 +0100

    .

--- src/nl/droidcon/myriver/MainActivity.java
+++ src/nl/droidcon/myriver/MainActivity.java
@@ -1,9 +1,48 @@
 package nl.droidcon.myriver;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private Mode mSelectedMode = Mode.RAND;
+
+    public enum Mode implements Parcelable {
+        PAPER,
+        ROCK,
+        SCISORS,
+        RAND;
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(final Parcel dest, final int flags) {
+            dest.writeInt(ordinal());
+        }
+
+        public static final Creator<Mode> CREATOR = new Creator<Mode>() {
+            @Override
+            public Mode createFromParcel(final Parcel source) {
+                return Mode.values()[source.readInt()];
+            }
+
+            @Override
+            public Mode[] newArray(final int size) {
+                return new Mode[size];
+            }
+        };
+    }
+
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +50,51 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        bindViews();
     }
+
+    public void onPaperSelected(View view) {
+        toast("paper");
+        mSelectedMode = Mode.PAPER;
+    }
+
+    public void onRockSelected(View view) {
+        toast("rock");
+        mSelectedMode = Mode.ROCK;
+    }
+
+    public void onScisorsSelected(View view) {
+        toast("scisors");
+        mSelectedMode = Mode.SCISORS;
+    }
+
+    public void onRandSelected(View view) {
+        toast("random");
+        mSelectedMode = Mode.RAND;
+    }
+
+    private void showDetector() {
+        Bundle extras = new Bundle();
+        extras.putParcelable("mode", mSelectedMode);
+
+        Intent intent = new Intent(MainActivity.this, ShakeDetectorActivity.class);
+        intent.putExtras(extras);
+        startActivity(intent);
+    }
+
+    private void toast(String log) {
+        Toast.makeText(this, log, 0).show();
+    }
+
+    private void bindViews() {
+        mPaper = (Button) findViewById(R.id.paper);
+        mRock = (Button) findViewById(R.id.paper);
+        mScrisors = (Button) findViewById(R.id.paper);
+        mRandom = (Button) findViewById(R.id.paper);
+    }
+
+    private Button mPaper;
+    private Button mRock;
+    private Button mScrisors;
+    private Button mRandom;
 }
---
+++ src/nl/droidcon/myriver/ShakeDetectorActivity.java
@@ -0,0 +1,18 @@
+package nl.droidcon.myriver;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Write comment here.
+ * <p/>
+ * Creator: <lukasz.izmajlowicz@mydriver.com>
+ * Date: 24/11/13
+ */
+public class ShakeDetectorActivity extends Activity  {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shake);
+    }
+}
